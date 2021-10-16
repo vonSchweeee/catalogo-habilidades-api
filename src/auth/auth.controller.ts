47936680f9f8b88c641fd0416208869ea72bbb16
José Vitor Almeida <jvitor_2003@hotmail.com>
2021-10-16 00:26:27 -0300

    Endpoint de login

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,5 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, HttpCode, Post } from '@nestjs/common';
+import { LoginUserDto } from 'src/users/dto/login-user.dto';
 import { RegisterUserDto } from 'src/users/dto/register-user.dto';
 import { AuthService } from './auth.service';
 
@@ -9,5 +10,11 @@ export class AuthController {
   @Post('register')
   async register(@Body() registerUserDto: RegisterUserDto) {
     return await this.authService.register(registerUserDto);
+  }
+
+  @Post('login')
+  @HttpCode(200)
+  async login(@Body() loginUserDto: LoginUserDto) {
+    return await this.authService.login(loginUserDto);
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,17 +1,18 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { RegisterUserDto } from 'src/users/dto/register-user.dto';
 import { User } from 'src/users/user.entity';
 import { Repository } from 'typeorm';
 
 import * as bcrypt from 'bcrypt';
+import { LoginUserDto } from 'src/users/dto/login-user.dto';
 
 @Injectable()
 export class AuthService {
   constructor(
     @InjectRepository(User) private usersRepository: Repository<User>
   ) { }
-
+    
   async register(registerUserDto: RegisterUserDto) {
     const hashPassword = await bcrypt.hash(registerUserDto.password, 10);
     const user = new User({
@@ -20,5 +21,23 @@ export class AuthService {
       name: registerUserDto.name
     });
     return this.usersRepository.save(user);
+  }
+
+  async login(loginUserDto: LoginUserDto) {
+    const user = await this.usersRepository.findOne(undefined, {
+      where: {email: loginUserDto.email}
+    });
+
+    if (user) {
+      const isMatch = await bcrypt.compare(loginUserDto.password, user.password);
+      if (isMatch) {
+        return user;
+      } else {  
+        throw new HttpException('Senha incorreta!', HttpStatus.FORBIDDEN);
+      }
+    } 
+    else {
+      throw new HttpException('Usuario não encontrado!', HttpStatus.NOT_FOUND);
+    }
   }
 }
